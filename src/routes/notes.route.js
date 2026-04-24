caa20f33d142ac8c235b1ed723dcc3fe46d2d067
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-04-24 10:38:11 +0530

    notes relocate

--- src/routes/notes.route.js
+++ src/routes/notes.route.js
@@ -26,25 +26,34 @@ const {
 
 
 // CRUD Bulk Routes
-router.post('/', createNote);
 router.post('/bulk', createBulkNotes);
-router.get('/', getAllNotes);
-router.get('/:id', getNoteById);
-router.put('/:id', replaceNote);
-router.patch('/:id', updateNote);
-router.delete('/:id', deleteNote);
 router.delete('/bulk', deleteBulkNotes);
+
+// Route Param Sections
 router.get('/category/:category', getNotesByCategory);
 router.get('/status/:isPinned', getNotesByStatus);
-router.get('/:id/summary', getNoteSummary);
+
+// Query Param Sections
 router.get('/filter', filterNotes);
 router.get('/filter/pinned', getPinnedNotes);
 router.get('/filter/category', filterByCategory);
 router.get('/filter/date-range', filterByDateRange);
+
+// Pagination
 router.get('/paginate', paginateNotes);
 router.get('/paginate/category/:category', paginateByCategory);
+
+// Sorting
 router.get('/sort', sortNotes);
 router.get('/sort/pinned', sortPinnedNotes);
 
+// CRUD Single Item Routes
+router.post('/', createNote);
+router.get('/', getAllNotes);
+router.get('/:id/summary', getNoteSummary);
+router.get('/:id', getNoteById);
+router.put('/:id', replaceNote);
+router.patch('/:id', updateNote);
+router.delete('/:id', deleteNote);
 
 module.exports = router;    
